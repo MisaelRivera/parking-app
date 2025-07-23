a4e923217c91e5ce4a74b5424b49c67eeba22827
Misael Rivera <misaelrivera121193@gmail.com>
2025-07-23 17:36:48 -0600

    Finishing user profile endpoints and starting vehicles crud endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 use \App\Http\Controllers\Api\V1\Auth;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
+use App\Http\Controllers\Api\V1\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,12 @@ Route::get('/user', function (Request $request) {
 
 Route::post('v1/auth/register', RegisterController::class);
 Route::post('v1/auth/login', Auth\LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('v1/profile', [Auth\ProfileController::class, 'show']);
+    Route::put('v1/profile', [Auth\ProfileController::class, 'update']);
+    Route::put('v1/password', Auth\PasswordUpdateController::class);
+    Route::delete('v1/logout', Auth\LogoutController::class);
+
+    Route::apiResource('v1/vehicles', VehicleController::class);
+});
